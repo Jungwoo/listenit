d78ee323bb13b554a8827874516fd7e7e42c427a
Jungwoo <travis0722@gmail.com>
2013-05-21 18:35:00 +0900

    no cover image, no book search result
    all done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
-  def safe_image_tag(source, options = {})
+  def safe_image_tag(source, size, options = {})
     if source != nil and source != "" then
     	image_tag(source, options)
+    elsif size == "small" then
+      image_tag("no-photo.gif", options)
+    elsif sisze == "large" then
+      image_tag("no-book-cover.png", options)
     end
   end 
 end
