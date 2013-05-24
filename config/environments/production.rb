ecc060c1bd5e4a64cb628a501e8f8818e79b8cb7
Jungwoo <travis0722@gmail.com>
2013-05-24 15:46:31 +0900

    Precompile config setting
    Add *.png, *.gif

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Bookmap::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile = ['*.svg','*.js', '*.css']
+  config.assets.precompile = ['*.png', '*.gif', '*.svg','*.js', '*.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
